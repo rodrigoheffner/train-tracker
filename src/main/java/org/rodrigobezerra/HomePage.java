b20cc96416ca0bf04744d1b9ac19972a928fdbd6
rodrigoheffner <rodrigo.heffner@gmail.com>
2016-04-30 18:06:12 +0100

    Tooltips, better styling
    Added tooltips to station names and twitter bootstrap styles to station
    codes.

--- src/main/java/org/rodrigobezerra/HomePage.java
+++ src/main/java/org/rodrigobezerra/HomePage.java
@@ -3,6 +3,7 @@ package org.rodrigobezerra;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
@@ -24,14 +25,50 @@ public class HomePage extends WebPage {
             protected void populateItem(ListItem<TrainRoute> li) {
                 li.setOutputMarkupId(true);
                 
-                li.add(new Label("from", li.getModelObject().getFrom()));
-                li.add(new Label("to", li.getModelObject().getTo()));
+                li.add(new Label("from", li.getModelObject().getFrom())
+                        .add(getClassAppender(li.getModelObject().getFrom()))
+                        .add(getTitleAppender(li.getModelObject().getFrom())));
+                li.add(new Label("to", li.getModelObject().getTo())
+                        .add(getClassAppender(li.getModelObject().getTo()))
+                        .add(getTitleAppender(li.getModelObject().getTo())));
                 li.add(new Label("std", li.getModelObject().getTrainTime().getStandardDepartureTime()));
                 li.add(new Label("eta", li.getModelObject().getTrainTime().getEstimatedDepartureTime()));
-//                li.add(new Label("response", li.getModelObject().getTrainTime().getResponse()));
+                
                 WebMarkupContainer responseLink = new WebMarkupContainer("response");
                 responseLink.add(new AttributeAppender("data-content", li.getModelObject().getTrainTime().getResponse()));
                 li.add(responseLink);
+            }
+
+            private AttributeAppender getClassAppender(String stationCode) {
+                String className = "label label-";
+                
+                if (stationCode.equals(Constants.STATION_CODE__BRIDGEND)) {
+                    className = className + "default";
+                } else if (stationCode.equals(Constants.STATION_CODE__CARDIFF_CENTRAL)) {
+                    className = className + "warning";
+                } else if (stationCode.equals(Constants.STATION_CODE__CARDIFF_QUEEN_STREET)) {
+                    className = className + "success";
+                } else if (stationCode.equals(Constants.STATION_CODE__LLANTWIT_MAJOR)) {
+                    className = className + "primary";
+                }
+                
+                return new AttributeAppender("class", className);
+            }
+
+            private AttributeAppender getTitleAppender(String stationCode) {
+                String tooltipValue = null;
+                
+                if (stationCode.equals(Constants.STATION_CODE__BRIDGEND)) {
+                    tooltipValue = "Bridgend";
+                } else if (stationCode.equals(Constants.STATION_CODE__CARDIFF_CENTRAL)) {
+                    tooltipValue = "Cardiff Central";
+                } else if (stationCode.equals(Constants.STATION_CODE__CARDIFF_QUEEN_STREET)) {
+                    tooltipValue = "Cardiff Queen Street";
+                } else if (stationCode.equals(Constants.STATION_CODE__LLANTWIT_MAJOR)) {
+                    tooltipValue = "Llantwit Major";
+                }
+                
+                return new AttributeAppender("title", tooltipValue);
             }
         });
 
